dedaccfce7642a09aa784497be32373b359f4da8
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-09-24 14:31:11 +0300

    change fonts paths

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,18 +7,21 @@ const srcFolder = "./src";
 export const path = {
   build: {
     files: `${buildFolder}/files/`,
+    assets: `${buildFolder}/assets/`,
     html: `${buildFolder}/`,
     css: `${buildFolder}/css/`,
     js: `${buildFolder}/js/`,
   },
   src: {
     files: `${srcFolder}/files/**/*.*`,
+    assets: `${srcFolder}/shared/assets/**/*.*`,
     html: `${srcFolder}/pages/*/*.html`,
     scss: `${srcFolder}/pages/*/*.scss`,
     js: `${srcFolder}/pages/*/*.js`,
   },
   watch: {
     files: `${srcFolder}/files/**/*.*`,
+    assets: `${srcFolder}/shared/assets/**/*.*`,
     html: `${srcFolder}/**/*.html`,
     scss: `${srcFolder}/**/*.scss`,
     js: `${srcFolder}/**/*.js`,
