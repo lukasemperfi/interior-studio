2445fe138355f451818514f72505d86f86a15a16
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2025-09-24 14:33:27 +0300

    change assets gulp config

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,4 +1,6 @@
-export const copy = () =>{
-    return app.gulp.src(app.path.src.files)
-        .pipe(app.gulp.dest(app.path.build.files))
-}
+export const copy = () => {
+  return app.gulp
+    .src([app.path.src.files, app.path.src.assets])
+    .pipe(app.gulp.dest(app.path.build.files))
+    .pipe(app.gulp.dest(app.path.build.assets));
+};
